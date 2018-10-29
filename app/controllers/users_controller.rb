0028058cd737ded2739cc7c272c9e0fc332acddc
Jonas Erthal <erthaljonas@gmail.com>
2018-10-29 18:02:58 -0400

    User creation w/ safe user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful save
+      flash[:success] = "Welcome to Friend-O!"
+      redirect_to root
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                  :password_confirmation)
+    end
+
 end
